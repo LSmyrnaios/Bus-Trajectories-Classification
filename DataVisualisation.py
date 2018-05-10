7e41badbcb99f22ec18e8caf4c248e3149c3251f
LSmyrnaios <lsmyrnaios@gmail.com>
2018-05-10 17:59:00 +0300

    - Correct maps' output-format. - Correct maps' centralization. - Choose 5 "random" trips from the new trainSet. - Update "train_set.csv".
    Co-authored-by: petros morfiris <peterthunder@gitlab.com>

--- DataVisualisation.py
+++ DataVisualisation.py
@@ -1,7 +1,7 @@
 import gmplot
 import pandas as pd
+import numpy as np
 from ast import literal_eval
-
 
 
 def data_visualization():
@@ -16,29 +16,35 @@ def data_visualization():
     #print trainSet['Trajectory']
 
     trip_num = 0
+    i=0
     for row in trainSet['Trajectory']:
-        trip_num += 1
         #print trip_num, row
+
+        if(trip_num==5):break
 
         timeStamps = []
         longtitutes = []
         latitudes = []
-        for trajectories in row:
-            timeStamps.append(trajectories[0])
-            #print timeStamps
-            longtitutes.append(trajectories[1])
-            #print longtitutes
-            latitudes.append(trajectories[2])
-            #print latitudes
+        if(i%7==1):
+            for trajectories in row:
+                timeStamps.append(trajectories[0])
+                #print timeStamps
+                longtitutes.append(trajectories[1])
+                #print longtitutes
+                latitudes.append(trajectories[2])
+                #print latitudes
 
-        gmap = gmplot.GoogleMapPlotter(latitudes[(len(latitudes)/2)+1], longtitutes[(len(longtitutes)/2)+1], 0)
+            gmap = gmplot.GoogleMapPlotter(np.mean(latitudes), np.mean(longtitutes), 12)
 
-        gmap.plot(latitudes, longtitutes, 'cornflowerblue', edge_width=10)
-        # gmap.scatter(more_lats, more_lngs, '#3B0B39', size=40, marker=False)
-        # gmap.scatter(marker_lats, marker_lngs, 'k', marker=True)
-        # gmap.heatmap(heat_lats, heat_lngs)
+            gmap.plot(latitudes, longtitutes, 'cornflowerblue', edge_width=10)
+            # gmap.scatter(more_lats, more_lngs, '#3B0B39', size=40, marker=False)
+            # gmap.scatter(marker_lats, marker_lngs, 'k', marker=True)
+            # gmap.heatmap(heat_lats, heat_lngs)
 
-        gmap.draw("Resources/maps/TripMap" + trip_num.__str__() + ".png")
+            gmap.draw("Resources/maps/TripMap" + i.__str__() + ".html")
+
+            trip_num += 1
+        i += 1
 
 
 if __name__ == '__main__':
